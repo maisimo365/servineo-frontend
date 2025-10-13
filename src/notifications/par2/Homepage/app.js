59dd446e21e26e30ad87c6a78c77ef859ab849f9
Thomas Sebasthian Mamani Condarco <202302149@est.umss.edu>
2025-10-13 16:03:09 -0400

    feat: add homepage for simulation environment

---
+++ src/notifications/par2/Homepage/app.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.getElementById("btnIngresar");
+  btn.addEventListener("click", () => {
+    alert("Bienvenido al entorno de simulación 🚀");
+  });
+});
